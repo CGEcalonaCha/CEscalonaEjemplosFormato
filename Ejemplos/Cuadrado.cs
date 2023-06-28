8a316bc02e1c45e62400e925a85e10885ebeeedc
Cecilia Gabriela Escalona Chavarria <cescalonacha@gmail.com>
2023-06-28 17:04:34 -0600

    Add project files.

---
+++ Ejemplos/Cuadrado.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejemplos
+{
+    ///Herencia 
+    public class Cuadrado : Figuras_Geometricas
+    {
+        ///Instancia de una clase 
+        Figuras_Geometricas Figuras_Geometricas = new Figuras_Geometricas();
+        /// Polimorfismo
+        public string CalcularArea()
+        {
+            return "Calcula el area triangulo";
+        }
+        public string CulcularArea()
+        {
+            return "Calcula el area del Cuadrado";
+        }
+
+        //Abstracion
+        /// Objeto de la vida real Cuadrado
+        /// ////Metodos 
+        public void iCalcularArea()
+        {
+
+        }
+        public void CalcularPerimetro()
+        {
+
+        }
+        public void ObtenerTipo()
+        {
+
+        }
+        public void Obtener()
+        {
+
+        }
+        //////Atributos /// Propiedades 
+        public Decimal Area { get; set; }
+        public Decimal Lado { get; set; }
+        public Decimal Perimetro { get; set; }
+        public Decimal Tipo { get; set; }
+    }
+}
---
+++ Ejemplos/Figuras Geometricas.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejemplos
+{
+    ///Plantilla de metodos y atributos
+    public  class Figuras_Geometricas
+    {
+        ////Metodos 
+        public void CalcularArea()
+        {
+
+        }
+        public void CalcularPerimetro()
+        {
+
+        }
+        public void ObtenerTipo()
+        {
+
+        }
+        public void ObtenerLado()
+        {
+
+        }
+        //////Atributos
+        public Decimal Area { get; set; }
+        public Decimal Lado { get; set; }
+        public Decimal Perimetro { get; set; }
+        public Decimal Tipo { get; set; }
+        ///Cobnstructor 
+        private decimal area;
+        private decimal perimetro;
+        private decimal tipo;
+        private decimal lado;
+
+        //Sobrecarga Metodos
+        public void CalculaPerimetro()
+        {
+
+        }
+        public void CalculaPerimetro(decimal Perimetro)
+        {
+            
+        }
+        ////Sobre Carga Construtores 
+        public  string IPerimetro;
+        public decimal ICalculaPerimetro;
+        
+
+            }
+}
